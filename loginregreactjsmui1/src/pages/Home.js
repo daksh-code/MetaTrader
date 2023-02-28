50b41758a825388b20f8c54fdd4e87f26a5d7f6d
daksh <dagariyadaksh@gmail.com>
2023-02-28 16:25:48 +0530

    strategy added

--- loginregreactjsmui1/src/pages/Home.js
+++ loginregreactjsmui1/src/pages/Home.js
@@ -6,7 +6,7 @@ const Home = () => {
       <Grid item sm={10}>
         <h1>Shalom!!!!</h1>
         <hr />
-        <p>Welcome to MetaTrader, the platform that allows you to replicate trades from master trdaers to your TD Ameritrade account automatically. By signing up for our paid subscription, you can enter your TD Ameritrade credentials and our system will automatically replicate your trades. No need to manually enter trades, we take care of it for you. Sign up now and start replicating your trades with ease.</p>
+        <p>Welcome to MetaTrader, the platform that allows you to replicate trades from master traders to your TD Ameritrade account automatically. By signing up for our paid subscription, you can enter your TD Ameritrade credentials and our system will automatically replicate your trades. No need to manually enter trades, we take care of it for you. Sign up now and start replicating your trades with ease.</p>
       </Grid>
     </Grid>
   </>;
